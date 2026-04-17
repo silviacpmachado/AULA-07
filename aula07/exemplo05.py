b26280035e75740d3360df448ca7aad71f631675
silviacpmachado <silviacpmachado@gmail.com>
2026-04-16 21:51:21 -0300

    inicializa

--- aula07/exemplo05.py
+++ aula07/exemplo05.py
@@ -1,1 +1,24 @@
 #titulo, autor, ano de publicação, gênero e número de páginas
+livros = []
+for i in range(2):
+    print(f' \n----- Livro {i+1}----')
+    livro = {}
+    livro['titulo'] = input('Nome do livro: ')
+    livro['autor'] = input('Nome do autor: ')
+    livro['ano'] = int(input('Ano de publicação: '))
+    livro['genero'] = input('Gênero do livro: ')
+    livro['paginas'] = int(input('Número de Página: '))
+
+    livros.append(livro)
+    print('Livro cadastrado!!!')
+
+# MOSTRANDO
+    print('\n ---- Livros a partir de 2020 ----' )
+for l in livros:
+    if l['ano'] >= 2020:
+        # print(l)
+        print(f'Nome do livro: {l['nome']}')
+        print(f'Ano :{l['ano']}')
+
+
+
